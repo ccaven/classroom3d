1f717d958e36323de812409e31da4ac5af37b25a
xacer <chacecaven@outlook.com>
2023-05-29 13:57:20 -0400

    add documentation to desmos graph component

--- src/lib/interactives/DesmosGraph.svelte
+++ src/lib/interactives/DesmosGraph.svelte
@@ -1,17 +1,10 @@
-<!-- 
-    
-    TODO: Fix ID clash problem
-
-    Maybe prefix IDs with peerId?
-
--->
 
 <script lang="ts">
 
     import { HTML } from "@threlte/extras";
     import { onDestroy, onMount } from 'svelte';
 
-    import Desmos, { type ExpressionState, type GraphSettings } from 'desmos';
+    import Desmos, { type ExpressionState } from 'desmos';
     import type { Update } from "$lib/network/network-types";
     import { uid } from "$lib/helper/uid";
     import { useNetworker } from "$lib/network";
@@ -22,12 +15,12 @@
     
     type DesmosUpdate = Update<WithId<DesmosUpdateKey>, Desmos.ExpressionState>;
     
-    let clientTag = new Array(8).fill(0).map((e, i) => Math.random() * 10 | 0).join("");
+    let clientTag = new Array(8).fill(0).map((e, i) => Math.random() * 10 | 0).join("") + "___";
 
     let divEle: HTMLDivElement;
 
-    const id = uid();
-    const withId = <T extends DesmosUpdateKey>(s: T): WithId<T> => `${s}-${id}`;
+    const graphId = uid();
+    const withId = <T extends DesmosUpdateKey>(s: T): WithId<T> => `${s}-${graphId}`;
     
     const networker = useNetworker();
 
@@ -36,28 +29,35 @@
     let lastExpressions: Desmos.ExpressionState[] = [];
 
     function onAddState(expression: ExpressionState) {
-        if (expression.id?.startsWith(clientTag)) {
-            expression.id = expression.id.replace(clientTag + "___", "");
-        }
-        
-        console.log("Added expression", expression);
+        // If this is our expression coming back in,
+        // then it must have our prefix attached to it's
+        // id. If that is the case, then remove the prefix
+        // because our local copy of our own expressions
+        // do not contain the prefix.
+        expression.id = removePrefix(expression.id, clientTag);
 
         calculator.setExpression(expression);
-        lastExpressions = calculator.getExpressions();
-    }
-    function onRemoveState(expression: ExpressionState) {
-        if (expression.id?.startsWith(clientTag)) {
-            expression.id = expression.id.replace(clientTag + "___", "");
-        }
 
-        console.log("Removed expression", expression);
+        resetLastExpressions();
+    }
+
+    function onRemoveState(expression: ExpressionState) {
+        // If this is our expression coming back in,
+        // then it must have our prefix attached to it's
+        // id. If that is the case, then remove the prefix
+        // because our local copy of our own expressions
+        // do not contain the prefix.
+        expression.id = removePrefix(expression.id, clientTag);
 
         calculator.removeExpression({ id: expression.id || "" });
-        lastExpressions = calculator.getExpressions();
+
+        resetLastExpressions();
     }
 
     onMount(() => {
 
+        // Initialize the calculator
+        // TODO: Be more precise with what is allowed/not allowed
         calculator = Desmos.GraphingCalculator(divEle, {
             autosize: false,
             images: false,
@@ -66,31 +66,45 @@
             notes: true     
         });
 
-        lastExpressions = calculator.getExpressions();
-
-        /** TODO: Check for old/new expressions */
+        resetLastExpressions();
         
+        // This is the polling interval
+        // that checks for updates in the state
+        // of the local graph.
         setInterval(() => {
 
-            if (!networker.usePeerId()) return;
-
+            /**
+             * Compute a new expression check
+             */
             let newExpressions = calculator.getExpressions();
 
-            // Calculate differences
+            /**
+             * Computed expressions that appeared in the new check
+             * but were not in the old check.
+             */
             let addedExpressions = newExpressions.filter(u => {
                 return !lastExpressions.find(v => objectEquals(v, u));
             });
 
+            /**
+             * Computed expressions that do not appear in the new check
+             * but did appear in the old check.
+             */
             let removedExpressions = lastExpressions.filter(u => {
                 return !newExpressions.find(v => objectEquals(v, u)) && !addedExpressions.find(v => v.id == u.id);
             });
 
+            // Send a DesmosUpdate for each added expression
+            // NOTE: There SHOULD only be one added expression,
+            //       but some cases, such as adding a bunch of
+            //       sliders at once, multiple DesmosUpdate's
+            //       will be sent.
             addedExpressions.forEach(expression => {
-                console.log("Added expression", expression);
 
-                if (expression.id?.indexOf("___") == -1) {
-                    expression.id = clientTag + "___" + expression.id;
-                }
+                // If this is one of our expressions,
+                // meaning it does not contain a prefix
+                // then add our prefix before sending it out
+                expression.id = addPrefix(expression.id, clientTag);
 
                 networker.broadcast<DesmosUpdate>(
                     withId("desmos-add-state"), 
@@ -98,28 +112,45 @@
                 );
             });
 
+            // Send a DesmosUpdate for each removed expression
+            // NOTE: There SHOULD only be one removed expression
+            //       but some cases, such as deleting a bunch
+            //       of blank lines at once, multiple
+            //       DesmosUpdate's are sent.
             removedExpressions.forEach(expression => {
-                console.log("Removed expression", expression);
 
-                if (expression.id?.indexOf("___") == -1) {
-                    expression.id = clientTag + "___" + expression.id;
-                }
+                // If this is one of our expressions,
+                // meaning it does not contain a prefix
+                // then add our prefix before sending it out
+                expression.id = addPrefix(expression.id, clientTag);
 
-                networker.broadcast<DesmosUpdate>(withId("desmos-remove-state"), expression);
+                networker.broadcast<DesmosUpdate>(
+                    withId("desmos-remove-state"), 
+                    expression
+                );
             });
 
-            lastExpressions = calculator.getExpressions();
+            // Update the expression cache
+            // so differences are not counted twice
+            resetLastExpressions();
         }, 100);
     });
 
+    // Add the handlers to the networker
     networker.addGlobalHandler<DesmosUpdate>(withId("desmos-add-state"), onAddState);
     networker.addGlobalHandler<DesmosUpdate>(withId("desmos-remove-state"), onRemoveState);
 
+    // If the component is for some reason
+    // removed from the tree, remove its event handlers
     onDestroy(() => {
         networker.removeGlobalHandler<DesmosUpdate>(withId("desmos-add-state"), onAddState);
         networker.removeGlobalHandler<DesmosUpdate>(withId("desmos-remove-state"), onRemoveState);
     });
 
+    /**
+     * Create a cache to avoid computing the output for two identical inputs
+     * @param f The function to memoize
+     */
     function memoize<T, K>(f: (a: T) => K) {
         let mem = new Map<T, K>();
         return (a: T) => {
@@ -130,10 +161,46 @@
         };
     }
 
+    /**
+     * A memoized version of JSON.stringify
+     */
     const stringify = memoize(JSON.stringify);
 
+    /**
+     * Use JSON.stringify to determine if two objects are equal
+     * @param a - The first object
+     * @param b - The second object
+     */
     function objectEquals(a: any, b: any) {
         return stringify(a) == stringify(b);
+    }
+
+    /**
+     * Set the `lastExpressions` variable to the latest
+     * expressions in the calculator.
+     */
+    function resetLastExpressions() {
+        lastExpressions = calculator.getExpressions();
+    }
+
+    /**
+     * Remove a prefix if it exists, otherwise do nothing
+     * @param full - The full string
+     * @param prefix - The prefix to remove
+     * @returns The full string without the prefix
+     */
+    function removePrefix(full: string | undefined, prefix: string) {
+        return full?.startsWith(prefix) ? full.replace(prefix, "") : full;
+    }
+
+    /**
+     * Add a prefix if no prefix exists, otherwise do nothing
+     * @param full - The full string
+     * @param prefix - The prefix to remove
+     * @returns The full string with the prefix added
+     */
+    function addPrefix(full: string | undefined, prefix: string) {
+        return full?.indexOf("___") == -1 ? prefix + full : full;
     }
 
 </script>
